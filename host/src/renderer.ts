2be1f93c8168ace545b2c6ddfd17e54f27969293
Leo <formaggio@hotmail.co.uk>
2021-01-14 11:27:50 +0000

    This is chris' fault

--- host/src/renderer.ts
+++ host/src/renderer.ts
@@ -2,7 +2,7 @@ import * as display from './utils/display';
 import { playAudio } from "./utils/audio";
 let btn = document.getElementById("coolbutton");
 if(btn!= null){
-btn.addEventListener("click", (e:Event) => playAudio("https://cdn.discordapp.com/attachments/538766359819976737/798520157005807637/3e54cc690cfb4a3fa332c63ec09d9bd8.377600.wav"));
+btn.addEventListener("click", (e:Event) => playAudio("https://cdn.discordapp.com/attachments/538766359819976737/798520157005807637/3e54cc690cfb4a3fa332c63ec09d9bd8.377600.wav",playbac));
 }
 
 
