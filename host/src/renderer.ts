00858896507ce10588a43a9a518f8f07bce23aae
chr1s-b <16579127+chr1s-b@users.noreply.github.com>
2021-01-14 13:31:33 +0000

    pain

--- host/src/renderer.ts
+++ host/src/renderer.ts
@@ -13,8 +13,8 @@ let btn = document.getElementById("coolbutton");
 
 
 if(btn!= null){
-btn.addEventListener("click", (e:Event) => playAudio("https://cdn.discordapp.com/attachments/538766359819976737/798520157005807637/3e54cc690cfb4a3fa332c63ec09d9bd8.377600.wav",4));  
-btn.addEventListener("click", (e:Event) => doThing());
-
-        
+    btn.addEventListener("click", (e:Event) => {
+        playAudio("https://cdn.discordapp.com/attachments/538766359819976737/798520157005807637/3e54cc690cfb4a3fa332c63ec09d9bd8.377600.wav",4);  
+        doThing();
+    });       
 }
